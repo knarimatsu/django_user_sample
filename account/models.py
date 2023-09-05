a3f08f8cd7aa8053baec8a87ffa793bec1c3c9fe
knarimatsu <nariomasahisa@gmail.com>
2023-09-05 21:24:57 +0900

    feat: add create user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.UUIDField(default=uuid.uuid4, max_length=50, primary_key=True, editable=False, null=False)
+    password = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=50)
+    comment = models.CharField(max_length=100, null=True)
+    email = models.EmailField(max_length=100, null=True)
+    
+    def __str__(self) -> str:
+        return self.user_id
+    
