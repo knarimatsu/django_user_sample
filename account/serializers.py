971f21f16ab3eeceb361e9c801b8d418db9b716d
knarimatsu <nariomasahisa@gmail.com>
2023-09-06 22:09:16 +0900

    feat: login validator

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,7 @@
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_201_CREATED
 from rest_framework import serializers
-from .models import User
-
+from .models import User, AccessToken
+import hashlib
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -18,3 +19,27 @@ class RegisterSerializer(serializers.ModelSerializer):
                 email= validated_data['email'],
             )
             return user
+        
+class LoginSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = AccessToken
+        fields = ['email', 'password']
+        
+
+    email = serializers.CharField(max_length=255, write_only=True)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+    def validate(self, data):
+        email = data.get('email')
+        password = hashlib.sha256(data.get('password').encode()).hexdigest()
+        try:
+            user = User.objects.get(email=email)
+            re_password = user.password
+            if email == user.email:        
+                if password == re_password:
+                    return data
+                else:
+                    raise serializers.ValidationError("ログイン失敗")
+        except:
+            raise serializers.ValidationError({'detail': "Login FAILED", 'error': 1}, HTTP_400_BAD_REQUEST)
