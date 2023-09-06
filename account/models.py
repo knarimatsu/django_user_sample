c7e1aaece5a0bf5c0630974f9fc470a38b01ec34
knarimatsu <nariomasahisa@gmail.com>
2023-09-06 22:08:37 +0900

    feat: add generation accessToken

--- account/models.py
+++ account/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.utils import timezone
+
 import uuid
+import hashlib
+from datetime import timedelta
+
 
 # Create your models here.
 
@@ -8,9 +13,35 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     nickname = models.CharField(max_length=50)
     comment = models.CharField(max_length=100, null=True)
-    email = models.EmailField(max_length=100, null=True)
+    email = models.EmailField(max_length=100, null=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     
     def __str__(self) -> str:
         return self.user_id
     
+
+def in_30_days():
+    return timezone.now() + timedelta(days=30)
+
+class AccessToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=500)
+    access_datetime = models.DateTimeField(default=in_30_days)
+    
+    def str(self):
+        dt = timezone.localtime(self.access_datetime).strftime('%Y-%m-%d %H:%M:%S')
+        return self.user.user_id + '(' + dt + ') - ' + self.token 
+    
+    def create(user: User):
+        if AccessToken.objects.filter(user=user).exists():
+            AccessToken.objects.filter(user=user).delete()
+            
+        dt = timezone.now()
+        string = str(user.user_id) + user.password + dt.strftime('%Y-%m-%d %H:%M:%S')
+        hash = hashlib.sha1(string.encode('utf-8')).hexdigest()
+        token = AccessToken.objects.create(
+            user=user,
+            token=hash,
+            access_datetime=dt
+        )
+        return token
