290ac6f95f072e919e2dfa2113f23ac54efaa497
knarimatsu <nariomasahisa@gmail.com>
2023-09-06 22:09:55 +0900

    feat: add LoginView

--- account/views.py
+++ account/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
-from .models import User
-from .serializers import RegisterSerializer
+from .models import User, AccessToken
+from .serializers import RegisterSerializer, LoginSerializer
 
 import hashlib
 
@@ -29,3 +29,15 @@ class RegisterView(APIView):
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     
+class LoginView(GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = User.objects.get(email=serializer.validated_data['email'])
+            email = serializer.validated_data['email']
+            token = AccessToken.create(user)
+            return Response({'detail': "Login SUCCESS", 'token': token.token, 'error': 0}, status=HTTP_200_OK)
+        return Response({'detail': "Login FAILED", 'error': 1}, status=HTTP_400_BAD_REQUEST)
