f3e6267ebbd8a9176b4d90452caf6e09c50c00e8
knarimatsu <nariomasahisa@gmail.com>
2023-09-06 19:46:25 +0900

    feat: add new column

--- account/serializers.py
+++ account/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth.hashers import make_password
+
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['user_id', 'password', 'nickname','comment', 'email']
+        fields = ['user_id', 'password', 'nickname','comment', 'email', 'created_at']
         extra_kwargs = {'password': {'write_only': True}}
-        
+                
         def create(self, validated_data):
-            user = User.objects.create_user(
+            user = User.objects.create(
                 user_id=validated_data['user_id'],
-                password=make_password(validated_data['password']),
+                password=validated_data['password'],
                 nickname=validated_data['nickname'],
                 comment=validated_data['comment'],
                 email= validated_data['email'],
